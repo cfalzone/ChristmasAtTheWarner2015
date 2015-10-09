3bdde9f4bca25bf784426901c76e5e741c1a6b76
cfalzone <cfalzone@aquent.com>
2015-10-09 09:51:35 -0400

    update to scrollorama

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
     <script src="<?php bloginfo('template_directory'); ?>/js/jquery.scrollTo-1.4.3.1.min.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/jquery.scrollorama.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery.parallax.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/jquery.easing.1.3.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery.scrollorama.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/jquery.scrolldeck.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/js/jquery.parallax.js"></script>
 	<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
     <?php wp_footer(); ?>
   </body>
