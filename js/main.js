a0df4716eca3b06103ce58a71ac98662170b2362
cfalzone <cfalzone@aquent.com>
2015-11-13 14:29:39 -0500

    had to remove animations for iphone

--- js/main.js
+++ js/main.js
@@ -4,26 +4,10 @@ $(document).ready(function() {
 	$(window).resize(function() {
 		RepositionNav();
 	});
-
-	//$('#home').parallax("50%", 0, 0.1, true);
 	
 	var deck = new $.scrolldeck({
 		slides: '.slide',
 		buttons: '#nav li a',
 		easing: 'easeInOutExpo'
-	});
-
-	var scrollorama = $.scrollorama({ blocks: '.slide' });
-
-	scrollorama.animate('#home h3', {
-		delay: 5,
-		duration: 400,
-		property: 'opacity',
-		start: 0.1
-	}).animate('#tickets h3', {
-		delay: 400,
-		duration: 600,
-		property: 'zoom',
-		start: 2
 	});
 });
