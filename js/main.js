655a8925eba865f5becab71f8d7a45e1aa697977
cfalzone <cfalzone@aquent.com>
2015-11-18 16:09:29 -0500

    No more crazy JS and many improvements

--- js/main.js
+++ js/main.js
@@ -5,17 +5,22 @@ $(document).ready(function() {
 		RepositionNav();
 	});
 	
-	var deck = new $.scrolldeck({
-		slides: '.slide',
-		buttons: '#nav li a',
-		easing: 'easeInOutExpo'
+	$('#nav').localScroll({
+		queue:true,
+		duration:1000,
+		hash:true,
+	});
+
+	$('#nav a').click(function() {
+		$('#nav a').removeClass('active');
+		$(this).addClass('active');
 	});
 });
 
 function RepositionNav() {
 	var windowHeight = $(window).height(); //get the height of the window
 	var navHeight = $('#nav').height() / 2;
-	var windowCenter = (windowHeight / 2); 
+	var windowCenter = (windowHeight / 2);
 	var newtop = windowCenter - navHeight;
 	$('#nav').css({"top": newtop}); //set the new top position of the navigation list
 }
