af80f77a851688b54bbb88db6dfcc7fcc7a6e400
cfalzone <cfalzone@aquent.com>
2015-11-18 10:44:05 -0500

    Optimized images and layouts for mobile

--- js/main.js
+++ js/main.js
@@ -11,3 +11,11 @@ $(document).ready(function() {
 		easing: 'easeInOutExpo'
 	});
 });
+
+function RepositionNav() {
+	var windowHeight = $(window).height(); //get the height of the window
+	var navHeight = $('#nav').height() / 2;
+	var windowCenter = (windowHeight / 2); 
+	var newtop = windowCenter - navHeight;
+	$('#nav').css({"top": newtop}); //set the new top position of the navigation list
+}
